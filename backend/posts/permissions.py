8fd7d32107fb156a1e6fd1896bd52719399b3697
MustafaArif519 <mustafaarif519@gmail.com>
2023-05-19 13:50:07 -0400

    Added permission levels for admin and staff

--- backend/posts/permissions.py
+++ backend/posts/permissions.py
@@ -10,4 +10,8 @@ class IsAuthorOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
+        if request.user.is_staff:
+            return True
+        if request.user.groups.filter(name='admin').exists():
+            return True
         return obj.author == request.user
